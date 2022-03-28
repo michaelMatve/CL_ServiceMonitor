f64de04a1f796d3e7be1e4a468d0de007efe1488
michaelMatve <michael.matve@gmail.com>
2022-03-28 09:38:09 +0300

    notes

--- MWinAlgo.py
+++ MWinAlgo.py
@@ -2,12 +2,27 @@
 class MWinAlgo:
     def __init__(self, mdrow):
         self.my_drow = mdrow
+        self.mon_run = False
+        # list for all the servies< (time : dict<pid : name )>>
 
     def start(self, refresh_time):
         self.my_drow.write("the program start to work")
+        #load the file ".monitor.log" to the list
+
+        self.mon_run = True
+        #while(mon_run)
+        # dict now_servies= <pid:name>
+        #comper to the last
+        #write changes
+        #checksum ->w_check = checksum(dict now_sercies)
+        #write inscrept(time +""+ check+"" +dict(now_sercies))
+        #wait(refresh_time)
+
 
     def stop(self):
         self.my_drow.write("the program stop to work")
+
+        self.mon_run = False
 
     def comper(self, fdate, ftime , tdate, ttime):
         self.my_drow.write(f"{fdate} {ftime} comper to {tdate} {ttime}")
