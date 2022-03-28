84cb340734818267ac0d29b159927025b9ed77c5
EylonNaamat <eylonnaamat@gmail.com>
2022-03-28 17:42:12 +0300

    without encrypt

--- MWinAlgo.py
+++ MWinAlgo.py
@@ -1,8 +1,16 @@
+import wmi
+from datetime import datetime
+import csv
+import time
+import zlib
+from cryptography.fernet import Fernet
+
 
 class MWinAlgo:
     def __init__(self, mdrow):
         self.my_drow = mdrow
         self.mon_run = False
+        self.services_list = [] # [(date, {pid : pname})]
         # list for all the servies< (time : dict<pid : name )>>
 
     def start(self, refresh_time):
@@ -17,6 +25,118 @@ class MWinAlgo:
         #checksum ->w_check = checksum(dict now_sercies)
         #write inscrept(time +""+ check+"" +dict(now_sercies))
         #wait(refresh_time)
+        i = 0
+        while(i < 2):
+            self.compare_services() # compare between services - prints to user, and write in status_log
+            self.write_to_servicelist() # write into serviceList the last sample of services according to date and checksum
+            time.sleep(2)
+            i+=1
+
+
+
+    # def check_hacked(self):
+
+
+        # while(self.mon_run):
+
+
+    def check_sum(self, dict_to_encrypt):
+        checksum = 0
+        for pname in dict_to_encrypt.items():
+            c1 = 1
+            for a in pname:
+                c1 = zlib.adler32(bytes(repr(a), 'utf-8'), c1)
+            checksum = checksum ^ c1
+        print(checksum)
+        return checksum
+
+    def compare_services(self):
+        curr_time = datetime.now()
+        curr_service = {}
+        f = wmi.WMI()
+        change = False
+
+        if not self.services_list:
+            f2 = open("status_log.txt", "a")
+            f2.write(f"date: {curr_time}\n")
+            self.my_drow.write("new services:")
+            f2.write("new services:\n")
+            for process in f.Win32_Process():
+                curr_service[process.ProcessId] = process.Name
+                self.my_drow.write(f"{process.ProcessId} {process.Name}")
+                f2.write(f"{process.ProcessId} - {process.Name}\n")
+            checksum = self.check_sum(curr_service)
+            f2.write(f"checksum: {checksum}\n")
+            f2.close()
+            self.services_list.append((curr_time, curr_service))
+        else:
+            last_service = self.services_list[-1][1]
+            for process in f.Win32_Process():
+                curr_service[process.ProcessId] = process.Name
+                if process.ProcessId not in last_service:
+                    change = True
+
+            for curr_pid in curr_service.keys():
+                if curr_pid not in last_service:
+                    change = True
+
+            if not change:
+                self.my_drow.write("nothing has changed!:")
+            else:
+                f2 = open("status_log.txt", "a")
+                self.my_drow.write("new services:")
+                f2.write(f"date: {curr_time}\n")
+                f2.write("new services:\n")
+                for curr_pid in curr_service.keys():
+                    if curr_pid not in last_service:
+                        self.my_drow.write(f"{curr_pid} - {curr_service[curr_pid]}")
+                        f2.write(f"{curr_pid} - {curr_service[curr_pid]}\n")
+
+                self.my_drow.write("services no longer run:")
+                f2.write("services no longer run:\n")
+                for last_pid in last_service.keys():
+                    if last_pid not in curr_service:
+                        self.my_drow.write(f"{last_pid} - {last_service[last_pid]}")
+                        f2.write(f"{last_pid} - {last_service[last_pid]}\n")
+                checksum = self.check_sum(curr_service)
+                f2.write(f"checksum: {checksum}\n")
+                f2.close()
+
+        self.services_list.append((curr_time,curr_service))
+
+    def write_to_servicelist(self):
+        # file look like :
+        # date, checksum
+        # actualdate, calc_checksum
+        # pid, pname
+        #actual_pid, pname
+
+        date = self.services_list[-1][0]
+        last_service = self.services_list[-1][1]
+        checksum = self.check_sum(last_service)
+
+        f = open("serviceList.csv", "a", newline="")
+        # f1 = open("serviceList_enc.csv", "a", newline="")
+        writer = csv.writer(f)
+        # writer1 = csv.writer(f1)
+
+        tup1 = ("date", "checksum")
+        writer.writerow(tup1)
+        # writer1 = csv.writer(tup1)
+
+        tup1 = (date, checksum)
+        writer.writerow(tup1)
+        # writer1 = csv.writer(tup1)
+
+        tup1 = ("pid", "pname")
+        writer.writerow(tup1)
+
+        for pid in last_service.keys():
+            tup1 = (pid, last_service[pid])
+            writer.writerow(tup1)
+
+        f.close()
+
 
 
     def stop(self):
---
+++ main.py
@@ -0,0 +1,58 @@
+# import wmi
+#
+# # Initializing the wmi constructor
+# f = wmi.WMI()
+#
+# # Printing the header for the later columns
+# print("pid   Process name")
+#
+# # Iterating through all the running processes
+# for process in f.Win32_Process():
+#     # Displaying the P_ID and P_Name of the process
+#     print(f"{process.ProcessId} {process.Name}")
+
+# import zlib
+#
+# d={'key1':'value1','key2':'value2'}
+# checksum=0
+# for item in d.items():
+#     c1 = 1
+#     for t in item:
+#         c1 = zlib.adler32(bytes(repr(t),'utf-8'), c1)
+#     checksum=checksum ^ c1
+#
+# print(checksum)
+
+
+from cryptography.fernet import Fernet
+
+# we will be encryting the below string.
+message = "hello geeks"
+
+# generate a key for encryptio and decryption
+# You can use fernet to generate
+# the key or use random key generator
+# here I'm using fernet to generate key
+
+key = Fernet.generate_key()
+
+# Instance the Fernet class with the key
+
+fernet = Fernet(key)
+
+# then use the Fernet class instance
+# to encrypt the string string must must
+# be encoded to byte string before encryption
+encMessage = fernet.encrypt(message.encode())
+
+print("original string: ", message)
+print("encrypted string: ", encMessage)
+
+# decrypt the encrypted string with the
+# Fernet instance of the key,
+# that was used for encrypting the string
+# encoded byte string is returned by decrypt method,
+# so decode it to string with decode methods
+decMessage = fernet.decrypt(encMessage).decode()
+
+print("decrypted string: ", decMessage)
